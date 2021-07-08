1e2f75ced243c50b8cdec758d341ad3e0fb9b650
divergent99 <abhineetsharma77@gmail.com>
2021-07-08 12:33:59 +0530

    local repo changes

---
+++ tha_day17/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="header">FoodItem</div>
+      <div className="container">
+        Burgers
+        </div>
+        <div className="inside">
+          CALORIE COUNT
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
